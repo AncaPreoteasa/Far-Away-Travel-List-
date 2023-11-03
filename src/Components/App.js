6f90b3dd36d69fedb51d8bd26a0ee15796df5573
Anca Preoteasa <114996344+AncaPreoteasa@users.noreply.github.com>
2023-11-03 12:21:28 +0200

    Far Away Travel List Project

---
+++ src/Components/App.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import Packinglist from "./Packinglist";
+import Stats from "./Stats";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 2, packed: true },
+];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearItems(item) {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems((items) => []);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <Packinglist
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearItems={handleClearItems}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
