0dc5bf6def9f1f49db5dc2fa17c4c0575d53f028
Anca Preoteasa <114996344+AncaPreoteasa@users.noreply.github.com>
2024-01-25 18:17:14 +0200

    Tested that selecting a sorting option changes the sorting order

--- src/Components/App.test.js
+++ src/Components/App.test.js
@@ -45,3 +45,37 @@ test("clicking the clear button triggers onClearItems", () => {
 
   expect(onClearItemsMock).toHaveBeenCalled();
 });
+
+test("selecting a sorting option changes the sorting order", () => {
+  const items = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+    { id: 3, description: "Charger", quantity: 2, packed: true },
+  ];
+
+  render(
+    <Packinglist
+      items={items}
+      onDeleteItem={() => {}}
+      onToggleItem={() => {}}
+      onClearItems={() => {}}
+    />
+  );
+
+  const inputOrderItems = screen.getAllByRole("listitem");
+  expect(inputOrderItems.map((item) => item.textContent)).toEqual([
+    "2 Passports❌",
+    "12 Socks❌",
+    "2 Charger❌",
+  ]);
+
+  const sortByDropdown = screen.getByRole("combobox");
+  fireEvent.change(sortByDropdown, { target: { value: "description" } });
+
+  const descriptionOrderItems = screen.getAllByRole("listitem");
+  expect(descriptionOrderItems.map((item) => item.textContent)).toEqual([
+    "2 Charger❌",
+    "2 Passports❌",
+    "12 Socks❌",
+  ]);
+});
